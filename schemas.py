13755a85e84c49af034bb2f4a4e09e89e98c41c0
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-05-21 22:24:01 +0300

    Initial commit

---
+++ schemas.py
@@ -0,0 +1,108 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class UserBase(BaseModel):
+    email: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class ArtistBase(BaseModel):
+    name: str
+    description: str
+
+
+class ArtistCreate(ArtistBase):
+    pass
+
+
+class Artist(ArtistBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class GenreBase(BaseModel):
+    name: str
+
+
+class GenreCreate(GenreBase):
+    pass
+
+
+class Genre(GenreBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class AlbumBase(BaseModel):
+    title: str
+    description: str
+    artist_id: int
+
+
+class AlbumCreate(AlbumBase):
+    pass
+
+
+class Album(AlbumBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class SongBase(BaseModel):
+    title: str
+    text: str
+    album_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class SongCreate(SongBase):
+    genres: List[int]
+
+
+class Song(SongBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class FavoriteSongBase(BaseModel):
+    song_id: int
+    user_id: int
+
+
+class FavoriteSongCreate(BaseModel):
+    song_id: int
+
+
+class FavoriteSong(FavoriteSongBase):
+
+    class Config:
+        from_attributes = True
+
+
+
